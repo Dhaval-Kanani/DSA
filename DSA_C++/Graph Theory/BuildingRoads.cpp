aa56ca5e355dac093176f609246f18e2a63e4183
Dhaval Kanani <dhavalkanani90@gmail.com>
2024-03-13 00:19:56 +0530

    Problem 3 & 4 Graph Theory

---
+++ DSA_C++/Graph Theory/BuildingRoads.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(vector<int> adj[], vector<int> &vis, int u)
+{
+ vis[u] = 1;
+
+ for (auto x : adj[u])
+ {
+  if (vis[x] != 1)
+  {
+   dfs(adj, vis, x);
+  }
+ }
+}
+
+int main()
+{
+
+ int n, m, ans = 0;
+ cin >> n >> m;
+ vector<int> adj[n + 1];
+ for (int i = 1; i <= m; i++)
+ {
+  int u, v;
+  cin >> u >> v;
+  adj[u].push_back(v);
+  adj[v].push_back(u);
+ }
+
+ vector<int> vis(n + 1);
+ vector<int> roads;
+ for (int i = 1; i <= n; i++)
+ {
+  if (vis[i] != 1)
+  {
+   dfs(adj, vis, i);
+   ans++;
+   roads.push_back(i);
+  }
+ }
+
+ if (ans - 1)
+ {
+  cout << ans - 1 << endl;
+  for (int i = 0; i < (int)roads.size() - 1; i++)
+  {
+   cout << roads[i] << " " << roads[i + 1] << endl;
+  }
+ }
+ else
+  cout << 0 << endl;
+
+ return 0;
+}
---
+++ DSA_C++/Graph Theory/MessageRoute.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(vector<int> adj[], vector<int> &vis, int u)
+{
+ vis[u] = 1;
+
+ for (auto x : adj[u])
+ {
+  if (vis[x] != 1)
+  {
+   dfs(adj, vis, x);
+  }
+ }
+}
+
+int main()
+{
+
+ int n, m;
+ cin >> n >> m;
+ vector<int> adj[n + 1];
+ for (int i = 1; i <= m; i++)
+ {
+  int u, v;
+  cin >> u >> v;
+  adj[u].push_back(v);
+  adj[v].push_back(u);
+ }
+
+ vector<int> vis(n + 1);
+ vector<int> pre(n + 1);
+ queue<int> q;
+ q.push(1);
+ vis[1] = 1;
+ bool check = false;
+
+ while (q.size())
+ {
+  int u = q.front();
+  q.pop();
+
+  if (u == n)
+  {
+   check = true;
+   break;
+  }
+
+  for (auto x : adj[u])
+  {
+   if (vis[x] != 1)
+   {
+    q.push(x);
+    pre[x] = u;
+    vis[x] = 1;
+   }
+  }
+ }
+
+ vector<int> ans;
+ if (check)
+ {
+  int x = n;
+  ans.push_back(n);
+  while (x != 1)
+  {
+   ans.push_back(pre[x]);
+   x = pre[x];
+  }
+  cout << ans.size() << endl;
+  for (int i = ans.size() - 1; i >= 0; i--)
+   cout << ans[i] << " ";
+  cout << endl;
+ }
+ else
+  cout << "IMPOSSIBLE" << endl;
+
+ return 0;
+}
